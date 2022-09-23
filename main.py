2ee75acfa1fb6f5f8d976cc05bf24b432082b9ac
Ksebarkos <76246139+Ksebarkos@users.noreply.github.com>
2022-09-23 23:51:52 +0300

    Add files via upload
    Requirement to build and run project:
    
    python3
    fastapi
    uvicorn
    sqlalchemy
    alembic
    Run the project in localhost:8080 by executing:
    
    alembic upgrade head (only if you run the project for the first time, it executes the migrations and builds the database)
    python main.py (to start the server)
    Inspect the docs in localhost:8080/docs

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, Depends, Request, Header
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+import uvicorn
+import schema
+import errors
+from typing import List
+from db import get_db
+import logic
+
+app = FastAPI()
+
+
+@app.exception_handler(errors.JsonException)
+async def unicorn_exception_handler(
+    request: Request, exc: errors.JsonException
+):
+    return JSONResponse(
+        status_code=exc.code,
+        content={
+            "message": exc.message,
+            "detail": exc.details,
+            "data": exc.data,
+        },
+    )
+
+
+@app.get("/users/", response_model=List[schema.User])
+def get_users(db: Session = Depends(get_db), token: str = Header(None)):
+    return logic.get_users(db, token)
+
+
+@app.post("/users/", response_model=schema.User)
+def create_user(body: schema.UserCreate, db: Session = Depends(get_db)):
+    return logic.create_user(db, body)
+
+
+@app.put("/admin/validate-user/", response_model=schema.User)
+def validate_user(
+    body: schema.UserId, db: Session = Depends(get_db),
+    token: str = Header(None)
+):
+    return logic.validate_user(db, body.user_id, token)
+
+
+@app.post("/login/", response_model=schema.LoginToken)
+def login(
+    body: schema.LoginCredentials, db: Session = Depends(get_db),
+):
+    return logic.login(db, body)
+
+
+@app.get("/auctions/", response_model=List[schema.GetAuction])
+def get_auctions(
+    db: Session = Depends(get_db), token: str = Header(None)
+):
+    return logic.get_auctions(db, token)  # , page)
+
+
+@app.post("/register-auction/", response_model=schema.LoginToken)
+def create_auction(
+    body: schema.AuctionCreate, db: Session = Depends(get_db),
+):
+    return logic.create_auction(db, body)
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
---
+++ schema.py
@@ -0,0 +1,71 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    password: str
+    email: str
+    name: str
+    surname: str
+    phone: str
+    address: str
+    postcode: str
+    city: str
+    afm: str
+    role: str
+    validated: bool
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    email: str
+    name: str
+    surname: str
+    phone: str
+    address: str
+    postcode: str
+    city: str
+    afm: str
+    role: str
+
+
+class UserId(BaseModel):
+    user_id: int
+
+
+class LoginCredentials(BaseModel):
+    username: str
+    password: str
+
+
+class LoginToken(BaseModel):
+    token: str
+    role: str
+
+
+class GetAuction(BaseModel):
+    token: str
+    page: int
+
+
+class AuctionCreate(BaseModel):
+    token: str
+    name: str
+    category: list
+    currently = 0.0
+    buy_price: float
+    first_bid = 0.0
+    number_of_bids = 0
+    location: str
+    country: str
+    position: str
+    started: datetime.datetime = "0000-00-00[T]00:00"  # YYYY-MM-DD[T]HH:MM
+    ends: datetime.datetime = "0000-00-00[T]00:00"
+    description: str
+    photo: list
